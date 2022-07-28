f4ea2d19678c6de88cf041f56588cf23623470e3
Yusef <yusefpasha8980@gmail.com>
2022-07-28 17:06:43 +0430

    Check Internet Connection + GPS and Myket Problem

--- app/src/main/java/com/plcoding/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/plcoding/weatherapp/presentation/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.plcoding.weatherapp.presentation
 
 import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResultLauncher
@@ -16,28 +21,55 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
 import com.plcoding.weatherapp.presentation.ui.theme.DarkBlue
 import com.plcoding.weatherapp.presentation.ui.theme.DeepBlue
 import com.plcoding.weatherapp.presentation.ui.theme.WeatherAppTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private lateinit var locationManager: LocationManager
+    private var gpsEnabled = false
+    private var networkEnabled = false
 
     private val viewModel: WeatherViewModel by viewModels()
     private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         permissionLauncher = registerForActivityResult(
             ActivityResultContracts.RequestMultiplePermissions()
         ) {
-            viewModel.loadWeatherInfo()
+            if (gpsEnabled) {
+                viewModel.loadWeatherInfo()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Please Enable GPS!",
+                    Toast.LENGTH_LONG
+                ).show()
+                lifecycleScope.launchWhenStarted {
+                    delay(2000)
+                    startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                }
+            }
         }
-        permissionLauncher.launch(arrayOf(
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION,
-        ))
+
+        permissionLauncher.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
+
+        locationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
         setContent {
             WeatherAppTheme {
                 Box(
@@ -55,7 +87,7 @@ class MainActivity : ComponentActivity() {
                         Spacer(modifier = Modifier.height(16.dp))
                         WeatherForecast(state = viewModel.state)
                     }
-                    if(viewModel.state.isLoading) {
+                    if (viewModel.state.isLoading) {
                         CircularProgressIndicator(
                             modifier = Modifier.align(Alignment.Center)
                         )
@@ -71,5 +103,41 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        try {
+            gpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        } catch (e: Exception) {
+        }
+
+        try {
+            networkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        } catch (e: Exception) {
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            do {
+                if (gpsEnabled && networkEnabled) {
+                    launch(Dispatchers.Main) {
+                        if (!networkEnabled){
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Please Check the Internet Connection!",
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                        viewModel.loadWeatherInfo()
+                    }
+                    break
+                }
+            }while (!gpsEnabled)
+        }
+
     }
 }
